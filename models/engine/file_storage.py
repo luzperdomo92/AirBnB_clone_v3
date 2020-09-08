9dc377a9c641c87e614ef471f9a94ae5aa9ff402
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-08 10:48:03 -0500

    uptade file and db storage, added methods get and count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class name and its ID """
+        if cls is not None:
+            search_object = cls + "." + id
+            obj_dict = self.all(cls)
+            for objects in obj_dict:
+                if id in objects:
+                    return obj_dict[search_object]
+            return None
+
+    def count(self, cls=None):
+        """ Returns the number of objects in storage"""
+        obj_dict = self.all(cls)
+        return len(obj_dict)
