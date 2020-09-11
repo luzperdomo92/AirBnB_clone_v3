62789f458264f75e5fff645efdaefd6fda1ba40d
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-10 23:03:27 -0500

    fixed stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,10 +14,10 @@ def status():
 @app_views.route('/stats')
 def stats():
     """ retrieves the number of each objects by type"""
-    objects_by_type = {"amenities": storage.count("Amenity"),
-                       "cities": storage.count("City"),
-                       "places": storage.count("Place"),
-                       "reviews": storage.count("Review"),
-                       "states": storage.count("State"),
-                       "users": storage.count("User")}
+    objects_by_type = {"amenities": storage.count(Amenity),
+                       "cities": storage.count(City),
+                       "places": storage.count(Place),
+                       "reviews": storage.count(Review),
+                       "states": storage.count(State),
+                       "users": storage.count(User)}
     return jsonify(objects_by_type)
